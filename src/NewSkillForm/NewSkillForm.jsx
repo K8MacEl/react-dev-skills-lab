9f8d165bfb0aedc426cd9f278e83e37c3c583f9c
k8mcelhnaey <kqmcelhaney@gmail.com>
2024-03-05 11:15:02 -0700

    Add Skill JSX added

--- src/NewSkillForm/NewSkillForm.jsx
+++ src/NewSkillForm/NewSkillForm.jsx
@@ -2,9 +2,19 @@
 
 export default function NewSkillForm() {
     return (
-        
+
         <form action="skill">
-            Skill
+            <label htmlFor="NewSkill">Skill <input type="text" /></label>
+            <label htmlFor="SkillLevel">Level
+                <select name="SkillLevel" id="SkillLevel">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+            </label>
+            <button>ADD SKILL</button>
         </form>
     )
 }
